152f0e6d6deeaae5aee6c4a4f1ea363507d034a9
kirtan517 <kananikirtan73@gmail.com>
2023-03-07 07:34:40 -0500

    Created a simple database

---
+++ SimpleDatabase/SimpleDatabase/src/main/java/com/example/simpledatabase/Country.java
@@ -0,0 +1,21 @@
+package com.example.simpledatabase;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //...
+}
