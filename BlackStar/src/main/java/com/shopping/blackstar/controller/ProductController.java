d3496e7b9a8393c7b0640fb99307efd356f33181
kirtan517 <kananikirtan73@gmail.com>
2023-12-20 22:23:33 -0500

    added new project

---
+++ BlackStar/src/main/java/com/shopping/blackstar/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.shopping.blackstar.controller;
+
+public class ProdcutController {
+
+   public String search(){
+      return "search-result";
+   }
+}
---
+++ BlackStar/src/main/java/com/shopping/blackstar/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.shopping.blackstar.controller;public class UserController {
+}
